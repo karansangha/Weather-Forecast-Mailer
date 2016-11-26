02ef008cc9bdefdbe85ab0d0924f25da7ae2e6e8
Karan Sangha <iamkaransangha@gmail.com>
2016-11-26 01:04:49 -0800

    Get data from Open Weather Map
    And print the forecast :D

--- emailer.py
+++ emailer.py
@@ -1,3 +1,5 @@
+import requests
+
 def get_emails():
 
 
@@ -17,7 +19,6 @@ def get_emails():
 
 def get_schedule():
 
-
     try:
         schedule_file = open('schedule.txt', 'r')
         schedule = schedule_file.read()
@@ -27,11 +28,32 @@ def get_schedule():
 
     return schedule
 
+def get_weather_forecast():
+    url='http://api.openweathermap.org/data/2.5/weather?q=Orlando,fl&units=metric&appid=42c7c4285b12e98757f58c8a1bf99adc'
+    weather_request = requests.get(url)
+    weather_json = weather_request.json()
+
+    # print (weather_json)
+
+    description = weather_json['weather'][0]['description']
+    temp_min = weather_json['main']['temp_min']
+    temp_max = weather_json['main']['temp_max']
+    # print (description)
+    # print (temp_min, temp_max)
+
+    forecast = 'The Circus forecast for today is ' + description
+    forecast += ' with a high of ' + str(temp_max) + 'C'
+    forecast += ' and a low of ' + str(temp_min) + 'C'
+
+    print (forecast)
+
 def main():
     emails = get_emails()
     print (emails)
 
     schedule = get_schedule()
     print (schedule)
+
+    get_weather_forecast()
 
 main()
