b8e76ae17abe4f8fd1f057e03cb2d45c384cca07
Karan Sangha <iamkaransangha@gmail.com>
2016-11-26 00:39:28 -0800

    Split data and add to dictionary
    Read data from emails.txt -> Add it to a dictionary -> Print the
    dictionary

---
+++ emailer.py
@@ -0,0 +1,13 @@
+emails = {}
+
+try:
+    email_file = open('emails.txt', 'r')
+
+    for line in email_file:
+        (email, name) = line.split(',')
+        emails[email] = name.strip()
+
+except FileNotFoundError as err:
+    print(err)
+
+print (emails)
